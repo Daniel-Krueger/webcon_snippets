5cfbab0d7e27cedb678ab4e28c8717811872329d
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2023-07-04 22:29:03 +0200

    Added check for choose fields

---
+++ BPS2023_upgrade_checks/ChooseFields_without_displayname.sql
@@ -0,0 +1,116 @@
+SELECT WFD_ID, 'WFD_AttChoose1', WFD_AttChoose1
+FROM WFElements
+WHERE (WFD_AttChoose1 IS NOT NULL AND WFD_AttChoose1 <> '' AND CHARINDEX('#',WFD_AttChoose1) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose2', WFD_AttChoose2
+FROM WFElements
+WHERE (WFD_AttChoose2 IS NOT NULL AND WFD_AttChoose2 <> '' AND CHARINDEX('#',WFD_AttChoose2) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose3', WFD_AttChoose3
+FROM WFElements
+WHERE (WFD_AttChoose3 IS NOT NULL AND WFD_AttChoose3 <> '' AND CHARINDEX('#',WFD_AttChoose3) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose4', WFD_AttChoose4
+FROM WFElements
+WHERE (WFD_AttChoose4 IS NOT NULL AND WFD_AttChoose4 <> '' AND CHARINDEX('#',WFD_AttChoose4) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose5', WFD_AttChoose5
+FROM WFElements
+WHERE (WFD_AttChoose5 IS NOT NULL AND WFD_AttChoose5 <> '' AND CHARINDEX('#',WFD_AttChoose5) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose6', WFD_AttChoose6
+FROM WFElements
+WHERE (WFD_AttChoose6 IS NOT NULL AND WFD_AttChoose6 <> '' AND CHARINDEX('#',WFD_AttChoose6) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose7', WFD_AttChoose7
+FROM WFElements
+WHERE (WFD_AttChoose7 IS NOT NULL AND WFD_AttChoose7 <> '' AND CHARINDEX('#',WFD_AttChoose7) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose8', WFD_AttChoose8
+FROM WFElements
+WHERE (WFD_AttChoose8 IS NOT NULL AND WFD_AttChoose8 <> '' AND CHARINDEX('#',WFD_AttChoose8) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose9', WFD_AttChoose9
+FROM WFElements
+WHERE (WFD_AttChoose9 IS NOT NULL AND WFD_AttChoose9 <> '' AND CHARINDEX('#',WFD_AttChoose9) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttChoose10', WFD_AttChoose10
+FROM WFElements
+WHERE (WFD_AttChoose10 IS NOT NULL AND WFD_AttChoose10 <> '' AND CHARINDEX('#',WFD_AttChoose10) = 0)
+union all
+
+SELECT WFD_ID, 'WFD_AttPeople1', WFD_AttPeople1
+FROM WFElements
+WHERE (WFD_AttPeople1 IS NOT NULL AND WFD_AttPeople1 <> '' AND CHARINDEX('#',WFD_AttPeople1) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople2', WFD_AttPeople2
+FROM WFElements
+WHERE (WFD_AttPeople2 IS NOT NULL AND WFD_AttPeople2 <> '' AND CHARINDEX('#',WFD_AttPeople2) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople3', WFD_AttPeople3
+FROM WFElements
+WHERE (WFD_AttPeople3 IS NOT NULL AND WFD_AttPeople3 <> '' AND CHARINDEX('#',WFD_AttPeople3) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople4', WFD_AttPeople4
+FROM WFElements
+WHERE (WFD_AttPeople4 IS NOT NULL AND WFD_AttPeople4 <> '' AND CHARINDEX('#',WFD_AttPeople4) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople5', WFD_AttPeople5
+FROM WFElements
+WHERE (WFD_AttPeople5 IS NOT NULL AND WFD_AttPeople5 <> '' AND CHARINDEX('#',WFD_AttPeople5) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople6', WFD_AttPeople6
+FROM WFElements
+WHERE (WFD_AttPeople6 IS NOT NULL AND WFD_AttPeople6 <> '' AND CHARINDEX('#',WFD_AttPeople6) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople7', WFD_AttPeople7
+FROM WFElements
+WHERE (WFD_AttPeople7 IS NOT NULL AND WFD_AttPeople7 <> '' AND CHARINDEX('#',WFD_AttPeople7) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople8', WFD_AttPeople8
+FROM WFElements
+WHERE (WFD_AttPeople8 IS NOT NULL AND WFD_AttPeople8 <> '' AND CHARINDEX('#',WFD_AttPeople8) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople9', WFD_AttPeople9
+FROM WFElements
+WHERE (WFD_AttPeople9 IS NOT NULL AND WFD_AttPeople9 <> '' AND CHARINDEX('#',WFD_AttPeople9) = 0)
+
+UNION ALL
+
+SELECT WFD_ID, 'WFD_AttPeople10', WFD_AttPeople10
+FROM WFElements
+WHERE (WFD_AttPeople10 IS NOT NULL AND WFD_AttPeople10 <> '' AND CHARINDEX('#',WFD_AttPeople10) = 0)
