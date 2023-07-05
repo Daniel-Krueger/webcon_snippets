130b65e385ccbdf5cf22ade08a271e1caef870c5
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2023-07-05 21:11:59 +0200

    Added union all to check

--- BPS2023_upgrade_checks/ChooseFields_ItemList_without_displayname.sql
+++ BPS2023_upgrade_checks/ChooseFields_ItemList_without_displayname.sql
@@ -6,7 +6,7 @@ and FDD_Name = 'DET_Att1'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att1 is not null and DET_Att1 <> '' and CHARINDEX('#',DET_Att1) = 0)
-
+UNION ALL
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att2
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
 on FDD_ID = DCN_FDDID 
@@ -15,7 +15,7 @@ and FDD_Name = 'DET_Att2'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att2 is not null and DET_Att2 <> '' and CHARINDEX('#',DET_Att2) = 0)
-
+UNION ALL
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att3
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
 on FDD_ID = DCN_FDDID 
@@ -24,7 +24,7 @@ and FDD_Name = 'DET_Att3'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att3 is not null and DET_Att3 <> '' and CHARINDEX('#',DET_Att3) = 0)
-
+UNION ALL
 
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att4
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
@@ -35,7 +35,7 @@ join WFElementDetails
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att4 is not null and DET_Att4 <> '' and CHARINDEX('#',DET_Att4) = 0)
 
-
+UNION ALL
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att5
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
 on FDD_ID = DCN_FDDID 
@@ -44,7 +44,7 @@ and FDD_Name = 'DET_Att5'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att5 is not null and DET_Att5 <> '' and CHARINDEX('#',DET_Att5) = 0)
-
+UNION ALL
 
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att6
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
@@ -55,7 +55,7 @@ join WFElementDetails
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att6 is not null and DET_Att6 <> '' and CHARINDEX('#',DET_Att6) = 0)
 
-
+UNION ALL
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att7
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
 on FDD_ID = DCN_FDDID 
@@ -64,7 +64,7 @@ and FDD_Name = 'DET_Att7'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att7 is not null and DET_Att7 <> '' and CHARINDEX('#',DET_Att7) = 0)
-
+UNION ALL
 
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att8
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
@@ -75,7 +75,7 @@ join WFElementDetails
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att8 is not null and DET_Att8 <> '' and CHARINDEX('#',DET_Att8) = 0)
 
-
+UNION ALL
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att9
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
 on FDD_ID = DCN_FDDID 
@@ -84,7 +84,7 @@ and FDD_Name = 'DET_Att9'
 join WFElementDetails 
 on DCN_WFCONID = DET_WFCONID
 and (DET_Att9 is not null and DET_Att9 <> '' and CHARINDEX('#',DET_Att9) = 0)
-
+UNION ALL
 
 select DET_WFDID, DCN_Prompt, FDD_Name, DCN_WFCONID, DET_Att10
 FROM [dbo].[WFFieldDetailDefinitions] join [dbo].[WFDetailConfigs]
