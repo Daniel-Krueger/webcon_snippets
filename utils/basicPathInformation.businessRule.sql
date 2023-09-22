33f19ddb048a567b6efef3cdf84192fee5341873
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2023-09-22 10:45:32 +0200

    Corrected command

--- utils/basicPathInformation.businessRule.sql
+++ utils/basicPathInformation.businessRule.sql
@@ -1,16 +1,16 @@
 /* Merging all the rows into a single column and adding a leading [ and trailing ] to return a json array */
-select '['+
+select isnull('['+
    STUFF(
     (
       /* Creating the json structure and retrieving the color from the xml string*/
       select 
-            CHAR(123)
+           ', '+ CHAR(123)
             + '"id":'+ cast(Path_ID as varchar(10))
             +',"name":"'+replace(PATH_Name,'"','''')+'"'
             +',"color":"'+isnull(XmlData.value('(/data/@color)[1]','nvarchar(max)'),'default') +'"'
-            +',"title":"'+replace(isnull((select TRANS_Name from Translates join TranslateLanguages on Translates.TRANS_LANID = TranslateLanguages.LAN_ID and TRANS_ELEMID = PATH_ID and TRANS_OBJID = 9  and SUBSTRING(LAN_Name,1,2) = SUBSTRING('de-DE',1,2)  ),PATH_Name),'"','''')+'"'
+            +',"title":"'+replace(isnull((select TRANS_Name from Translates join TranslateLanguages on Translates.TRANS_LANID = TranslateLanguages.LAN_ID and TRANS_ELEMID = PATH_ID and TRANS_OBJID = 9  and SUBSTRING(LAN_Name,1,2) = SUBSTRING('{USERLAN}',1,2)  ),PATH_Name),'"','''')+'"'
             +CHAR(125)
-            +','
+            
       from 
             (
                   /* Returning the path information */
@@ -19,5 +19,5 @@ select '['+
             ) temp
      FOR XML PATH('')
   )
-  ,1,0,'')
-  +']'
+  ,1,2,'')
+  +']','[]')
