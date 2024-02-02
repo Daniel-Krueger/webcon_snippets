e60b1add433a04b124e5c86373168bb84f4cd4b5
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2024-02-02 20:45:00 +0100

    Updated parentLogic to support help files

--- ModalDialog/parentLogic.js
+++ ModalDialog/parentLogic.js
@@ -1,6 +1,10 @@
 window.ccls = window.ccls || {};
 ccls.modal = ccls.modal || {};
 ccls.modal.dialog = ccls.modal.dialog || {};
+// Either "workflowInstance" or "external"; We don't know the URL of a workflowInstance, as it may have been changed upon path transition.
+// Therefore we need to get the url from the child window via a post message.
+
+ccls.modal.dialog.replaceWindowBehavior;
 ccls.modal.dialog.startDebugger = function () {
   // Start debugger, if debug parameter is set and dev tools are started.
   if (new URLSearchParams(document.location.search).get("debug") == "modalDialogParent") {
@@ -12,26 +16,6 @@ ccls.modal.dialog.startDebugger = function () {
 ccls.modal.dialog.closingTimeout = null;
 //#region closing functions
 ccls.modal.dialog.closeFunctions = ccls.modal.dialog.closeFunctions || {};
-
-// All entered data may be lost
-ccls.modal.dialog.closeFunctions.displayAttachment = function (parameters) {
-  // parameters {"dbId":1,"elementId":123,"attachmentId":456};
-  if (parameters == null) {
-    console.log("No parameters have been provided");
-    return;
-  }
-
-  if (typeof (parameters.dbId) == "undefined") {
-    parameters.dbId = ccls.utils.getIdFromUrl("db");
-  }
-
-  if (!ccls.utils.continueAlsoPageIsDirty) return;
-
-  var url = `"/api/nav/db/${parameters.dbId}/element/${parameters.elementId}/attachments/${parameters.attachmentId}/edit?withRedirect=true"`;
-  console.log(`Opening edit document URL: '${url}'`);
-  window.location = url;
-}
-
 
 // This will trigger the refresh button in the tool bar
 // If you only need to reload datatables, there's no need to call this.
@@ -112,12 +96,20 @@ ccls.modal.dialog.closeFunctions.setGuidForItemListColumn = async function (para
 ccls.modal.dialog.customClosingFunction = undefined;
 ccls.modal.dialog.close = function (parameters) {
   ccls.modal.dialog.startDebugger();
+  document.removeEventListener("keydown", ccls.modal.dialog.closeOnEscape);
   document.getElementById("cclsModaliframe").contentWindow.postMessage(new Message("parentClosing", parameters));
   ccls.modal.dialog.closingTimeout = setTimeout(() => {
     console.log('Closing modal due to timeout');
     ccls.modal.dialog.childClosed(parameters);
   }, 100);
 };
+
+ccls.modal.dialog.closeOnEscape = function (event) {
+  if (event.key === "Escape") {
+    ccls.modal.dialog.close()
+  }
+}
+
 ccls.modal.dialog.childClosed = function (parameters) {
   clearTimeout(ccls.modal.dialog.closingTimeout);
   document.getElementById('cclsModal').style.display = "none";
@@ -134,8 +126,11 @@ ccls.modal.dialog.childClosed = function (parameters) {
   // switched from element.click()  to  setTimeout(() => { element.click() }, index * 122) and used a random "random" interval
   // executing the refresh button simultaneously  breaks the refresh as of BPS 2023 R2
   //  Needs to be revisited at later version.
-  $(".reload-button-container").find("button").each((index, element) => { element.click() });
-  $(".reload-button-container").find("span").each((index, element) => { element.click() });
+  // $(".reload-button-container").find("button").each((index, element) => { element.click() });
+  // $(".reload-button-container").find("span").each((index, element) => { element.click() });
+
+  $(".reload-button-container").find("button").each((index, element) => { setTimeout(() => { element.click() }, index * 122) });
+  $(".reload-button-container").find("span").each((index, element) => { setTimeout(() => { element.click() }, index * 99) });
   if (typeof (ccls.modal.dialog.customClosingFunction) == 'function') {
     ccls.modal.dialog.customClosingFunction(parameters);
   }
@@ -143,6 +138,26 @@ ccls.modal.dialog.childClosed = function (parameters) {
 //#endregion
 
 //#region functions to display the dialog
+ccls.modal.dialog.displayAttachment = function (title, parameters, dimensions) {
+  // parameters {"dbId":1,"attachmentId":456};
+  if (parameters == null) {
+    console.log("No parameters have been provided");
+    return;
+  }
+
+  if (typeof (parameters.dbId) == "undefined") {
+    parameters.dbId = ccls.utils.getIdFromUrl("db");
+  }
+
+  var url = `/attachments/db/${parameters.dbId}/preview/${parameters.attachmentId}?hash=` + Date.now();
+  console.log(`Display attachment: '${url}'`);
+  ccls.modal.dialog.internalOpen('"default":"' + title + '"', url, dimensions, null, false)
+}
+
+ccls.modal.dialog.displayExternalPage = function (title, url, dimensions) {
+  ccls.modal.dialog.startDebugger();
+  ccls.modal.dialog.internalOpen('"default":"' + title + '"', url, dimensions, null, false);
+}
 
 // parameter titleLabels: {"default": "Default title","de":"Title de","en":"Title en"}
 // parameter url: 
@@ -164,7 +179,7 @@ ccls.modal.dialog.displayWorkflow = function (titleLabels, urlParametersAsString
   }
   let urlSearchParams = ccls.modal.dialog.getSearchParams(searchParametersAsString);
   let url = `/embed/form/db/${urlParameters.dbId}/element/${urlParameters.elementId}/form?${urlSearchParams.toString()}&theme=${window.initModel.userTheme}`
-  ccls.modal.dialog.internalOpen(titleLabels, url, dimensions, closeFunction);
+  ccls.modal.dialog.internalOpen(titleLabels, url, dimensions, closeFunction, true);
 }
 
 // parameter titleLabels: {"default": "Default title","de":"Title de","en":"Title en"}
@@ -199,14 +214,15 @@ ccls.modal.dialog.startWorkflow = function (titleLabels, urlParametersAsString, 
   if (typeof (urlParameters.parentInstance) == "number") urlSearchParams.append("parent_wfdid", urlParameters.parentInstance);
 
   let url = `/embed/form/db/${urlParameters.dbId}/start/wf/${urlParameters.workflowId}/dt/${urlParameters.formTypeId}/form?${urlSearchParams.toString()}&theme=${window.initModel.userTheme}`
-  ccls.modal.dialog.internalOpen(titleLabels, url, dimensions, closeFunction);
+  ccls.modal.dialog.internalOpen(titleLabels, url, dimensions, closeFunction, true);
 };
 
-ccls.modal.dialog.internalOpen = function (titleLabels, url, dimensions, closeFunction) {
+ccls.modal.dialog.internalOpen = function (titleLabels, url, dimensions, closeFunction, opensWorkflowInstance) {
   console.log(url);
   let titles = JSON.parse("{" + titleLabels + "}");
   let title = titles[window.initModel.userLang.substr(0, 2)];
   if (typeof (title) == "undefined") title = titles.default;
+  ccls.modal.dialog.replaceWindowBehavior = typeof (opensWorkflowInstance) == "undefined" || opensWorkflowInstance == true ? "workflowInstance" : "external";
 
   // Display modal and set information
   document.getElementById("cclsModaliframe").src = url;
@@ -218,7 +234,7 @@ ccls.modal.dialog.internalOpen = function (titleLabels, url, dimensions, closeFu
     }
     $(".modal-window").attr("style", "z-index: 2003;" + dimensions);
   }
-
+  document.addEventListener("keydown", ccls.modal.dialog.closeOnEscape);
 
   if (typeof (closeFunction) == "function") {
     ccls.modal.dialog.customClosingFunction = closeFunction;
@@ -241,7 +257,7 @@ ccls.modal.dialog.getSearchParams = function (searchParametersAsString) {
   for (const parameter in searchParameters) {
     let parameterValue = searchParameters[parameter];
     if (parameterValue.toString().indexOf("WFD_") > -1) {
-      parameterValue = GetValue(parameterValue.replace("WFD_",""));
+      parameterValue = GetValue(parameterValue.replace("WFD_", ""));
     }
     urlSearchParams.append(parameter, encodeURIComponent(parameterValue))
   }
@@ -275,12 +291,23 @@ ccls.modal.dialog.toggleExpand = function () {
 
 ccls.modal.dialog.replaceWindow = function () {
   ccls.modal.dialog.startDebugger();
-  if (!ccls.utils.continueAlsoPageIsDirty()) return;
-  document.getElementById("cclsModaliframe").contentWindow.postMessage(new Message("sendURL", null));
+  //if (!ccls.utils.continueAlsoPageIsDirty()) return;
+  if (ccls.modal.dialog.replaceWindowBehavior == "workflowInstance") {
+    document.getElementById("cclsModaliframe").contentWindow.postMessage(new Message("sendURL", null));
+  }
+  else {
+    console.log("open iframe url in new tab");
+    window.open(document.getElementById("cclsModaliframe").src, "_blank");
+  }
+
 };
 //#endregion
 ccls.modal.dialog.dialogHtml =
-  `
+  `<style>
+  #cclsModalLayoutDefinition  > header {
+    cursor:move;
+  }
+  </style>
   <div id="cclsModal" class="modal-outer" style="display:none">
     <div
       id="cclsModalLayoutDefinition"
@@ -294,8 +321,8 @@ ccls.modal.dialog.dialogHtml =
         </div>
 
         <div class="actions">
-        <!-- reusing the expand class to match the styling, but moving the position of the element with the style -->
-          <div id="cclsOpenInNewWindow" class="expand" style="right: 60px;">
+        <!-- reusing the expand class to match the styling, but moving the position of the element with the style; depending on the media size, it's not visible therefore display block-->
+          <div id="cclsOpenInNewWindow" class="expand" style="right: 60px;display:block">
             <button
               class="webcon-button webcon-button--padding-default standard-focus webcon-button--icon-button no-hover no-background th-hover"
               aria-label="Close"
@@ -306,7 +333,7 @@ ccls.modal.dialog.dialogHtml =
          
             </button>
             </div>
-          <div id="cclsExpandDialog" class="expand">
+          <div id="cclsExpandDialog" class="expand" style="display:block">
             <button
               class="webcon-button webcon-button--padding-default standard-focus webcon-button--icon-button no-hover no-background th-hover"
               aria-label="Full screen"
@@ -355,6 +382,44 @@ $("#cclsExpandDialog > button").on('click', function () {
 $("#cclsOpenInNewWindow > button").on('click', function () {
   ccls.modal.dialog.replaceWindow();
 });
+
+
+ccls.modal.dialog.dragElement = function (element) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  element.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+ccls.modal.dialog.dragElement(document.querySelector("#cclsModalLayoutDefinition"));
 //#endregion 
 
 //#region Setting up communication between parent and child
@@ -373,24 +438,30 @@ if (typeof (ccls.modal.dialog.abortController) !== "undefined") {
 }
 ccls.modal.dialog.abortController = new AbortController();
 ccls.modal.dialog.parentMessageHandler = function (e) {
-  let data = e.data;
-  if (data.type == "fullUrl") {
-    if (data.body.url.indexOf("?") > -1) {
-      data.body.url += '&'
+  if (e.data) {
+    let data = e.data;
+    if (data.type == "fullUrl") {
+      if (data.body.url.indexOf("?") > -1) {
+        data.body.url += '&'
+      }
+      else {
+        data.body.url += '?'
+      }
+      let url = data.body.url + "returnUrl=" + encodeURIComponent(document.location.href);
+      window.open(url,"_blank");
     }
-    else {
-      data.body.url += '?'
-    }
-    document.location.href = data.body.url + "returnUrl=" + encodeURIComponent(document.location.href);
-  }
-  if (data.type == "childClosed") {
-    ccls.modal.dialog.childClosed(data.body)
+    if (data.type == "childClosed") {
+      ccls.modal.dialog.childClosed(data.body)
 
+    }
   }
 }
-document.addEventListener("keydown", ccls.modal.dialog.parentMessageHandler, { signal: ccls.modal.dialog.abortController.signal });
+window.addEventListener("message", ccls.modal.dialog.parentMessageHandler, { signal: ccls.modal.dialog.abortController.signal });
+
 
 //#endregion
 
 //the last line of a script must not be a comment
 console.log("modal dialog parent logic executed");
+
+
